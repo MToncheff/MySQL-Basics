61da29683108b34eb28fbaf565b9cd37bc0e50b6
MToncheff <47825748+MToncheff@users.noreply.github.com>
2019-09-13 16:55:43 +0300

    fix for 03, adding 04 , 05

--- DATA AGGREGATION/03. Longest Magic Wand Per Deposit Group.sql
+++ DATA AGGREGATION/03. Longest Magic Wand Per Deposit Group.sql
@@ -1,4 +1,3 @@
-SELECT DISTINCT deposit_group, MAX(magic_wand_size) AS 'longest_magic_land' From wizzard_deposits
+SELECT deposit_group, MAX(magic_wand_size) AS `longest_magic_land` From wizzard_deposits
 GROUP BY deposit_group
-ORDER BY magic_wand_size DESC, deposit_group
-
+ORDER BY `longest_magic_land` asc, deposit_group ASC
