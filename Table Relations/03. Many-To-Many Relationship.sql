3ef299d28c987ba945c95f7c07e5ad25407a5753
MToncheff <47825748+MToncheff@users.noreply.github.com>
2019-09-18 16:33:24 +0300

    Create 03. Many-To-Many Relationship.sql

---
+++ Table Relations/03. Many-To-Many Relationship.sql
@@ -0,0 +1,40 @@
+CREATE TABLE students
+(
+student_id INT(11) NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(50) NOT NULL
+);
+CREATE TABLE exams
+(
+exam_id INT(11) NOT NULL UNIQUE PRIMARY KEY,
+name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE students_exams
+(
+student_id INT(11) NOT NULL,
+exam_id INT(11) NOT NULL,
+
+PRIMARY KEY (student_id, exam_id),
+
+CONSTRAINT FK_student_id FOREIGN KEY (student_id)
+REFERENCES students(student_id),
+
+CONSTRAINT FK_exam_id FOREIGN KEY (exam_id)
+REFERENCES exams(exam_id)
+);
+
+INSERT INTO students(name) VALUE ('Mila');
+INSERT INTO students(name) VALUE ('Toni');
+INSERT INTO students(name) VALUE ('Ron');
+
+INSERT INTO exams(exam_id, name) VALUES (101, 'Spring MVC');
+INSERT INTO exams(exam_id, name) VALUES (102, 'Neo4j');
+INSERT INTO exams(exam_id, name) VALUES (103, 'Oracle 11g');
+
+INSERT INTO students_exams(student_id, exam_id) VALUES (1, 101);
+INSERT INTO students_exams(student_id, exam_id) VALUES (1, 102);
+INSERT INTO students_exams(student_id, exam_id) VALUES (2, 101);
+INSERT INTO students_exams(student_id, exam_id) VALUES (3, 103);
+INSERT INTO students_exams(student_id, exam_id) VALUES (2, 102);
+INSERT INTO students_exams(student_id, exam_id) VALUES (2, 103);
+
