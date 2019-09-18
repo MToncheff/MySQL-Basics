7f30635329c717822b37f958f41fbb13b1b8200f
MToncheff <47825748+MToncheff@users.noreply.github.com>
2019-09-18 15:36:22 +0300

    Update 18. Salary Challenge.sql

--- DATA AGGREGATION/18. Salary Challenge.sql
+++ DATA AGGREGATION/18. Salary Challenge.sql
@@ -1,8 +1,10 @@
-SELECT e.first_name, e.last_name, e.department_id FROM employees as e
-WHERE e.salary > 
-(SELECT AVG(em.salary)
-FROM employees AS em
-WHERE em.department_id = e.department_id
-)
-ORDER BY e.department_id
-LIMIT 10;
+select em.first_name, em.last_name, em.department_id
+from employees as em
+join
+(select e.department_id, avg(e.salary) as avg_salary
+from employees as e
+group by e.department_id) as avg_salaries
+on em.department_id = avg_salaries.department_id
+where em.salary > avg_salaries.avg_salary
+order by em.department_id
+limit 10
