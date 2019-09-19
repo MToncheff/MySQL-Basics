26ed82c0b5f7683317f3d7f2a71cedf70fceefbd
MToncheff <47825748+MToncheff@users.noreply.github.com>
2019-09-19 12:47:58 +0300

    Update 03. Sales Employee.sql

--- SUBQUERIES AND JOINS/03. Sales Employee.sql
+++ SUBQUERIES AND JOINS/03. Sales Employee.sql
@@ -1,6 +1,6 @@
 SELECT em.employee_id, em.first_name, em.last_name, d.name AS `department_name` 
 FROM employees em 
 JOIN departments d
-
+ON d.department_id = em.department_id
 WHERE d.name = 'Sales'
 ORDER BY employee_id DESC
